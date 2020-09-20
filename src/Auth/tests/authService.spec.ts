33d9946e5875fc91caab67bfa5949e642f3ee84d
Dmitrii Shehunov <53045487+Dmit0@users.noreply.github.com>
2020-09-20 21:15:19 +0300

    Update authService.spec.ts

--- src/Auth/tests/authService.spec.ts
+++ src/Auth/tests/authService.spec.ts
@@ -8,12 +8,7 @@ describe('UserService', () => {
    
     let userService:UserService;
 
-    const emptyUser=false
-    
-    const nonValidUser={
-        name:'',
-        password:'12414412'
-    }
+    const emptyUser=null
     
     const validUser={
         name:'qwer',
@@ -32,59 +27,64 @@ describe('UserService', () => {
       password:'32974732'
     }
 
-    
-
     class mockModel {
             constructor(public data?: any) {} 
             save() {
                 return this.data;
             }  
             static findOne() {
-                return ;
+                return
             }
-        }   
-    describe('signUp', () => {  
-        beforeEach(async() => { 
-                const moduleRef=await Test.createTestingModule({
-                    providers:[
-                        UserService,
-                        {
-                            provide:getModelToken(User.name),
-                            useValue:mockModel,
-                        }
-                    ]
-                }).compile()
-                userService=moduleRef.get<UserService>(UserService)
-            });
-
-        describe('sign up if user is not exist',()=>{
-            jest.spyOn(mockModel, 'findOne').mockImplementation(() => emptyUser);
+            static findOneAndDelete(){
+                return
+            }
+            static findOneAndUpdate(){
+                return
+            }
+        } 
+    beforeEach(async() => { 
+        const moduleRef=await Test.createTestingModule({
+            providers:[
+                UserService,
+                {
+                    provide:getModelToken(User.name),
+                    useValue:mockModel,
+                }
+            ]
+        }).compile()
+        userService=moduleRef.get<UserService>(UserService)
+    });   
+    
+        describe('signUp', () => {  
+       
             it('should create a user if you send valid data',async() => {
-                const response = await userService.signUp(validUser)
+                jest.spyOn(mockModel, 'findOne').mockImplementation(() => emptyUser);
+                const response = await  userService.signUp(validUser)
                 expect(response).toBeTruthy()
             });
-        })
-
-        describe('sign up if user exist',()=>{
-            jest.spyOn(mockModel, 'findOne').mockImplementation(() => sameUser);
             it('shouldnt create the same user',async() => {
+                jest.spyOn(mockModel, 'findOne').mockImplementation(() => sameUser);
                 const response = await userService.signUp(sameUser)
                 expect(response).toBeFalsy()
-            });
-        })  
-        
+            }); 
     });
 
-    // describe('signIn',()=>{
-    //     it('should return token if login is succes',async()=>{
-    //         const response = await userService.signIn(sameUser)
-    //         expect(response).not.toBeNull()
-    //     })
-    //     it('should return an error if some parametrs are wrong',async()=>{
-    //         const response = await userService.signIn(nonValidUser)
-    //         const response1 = await userService.signIn(validUser)
-    //         expect(response).toBeNull()
-    //         expect(response1).toBeNull()
-    //     })  
-    // })
+    describe('signIn',()=>{
+        it('should return token if login is succes',async()=>{
+            jest.spyOn(mockModel, 'findOne').mockImplementation(() => sameUser);
+            const response = await userService.signIn(sameUser)
+            expect(response).not.toBeNull()
+        })
+        it('should return an error if no such user',async()=>{
+            jest.spyOn(mockModel, 'findOne').mockImplementation(() => emptyUser);
+            const response1 = await userService.signIn(validUser)
+            expect(response1).toBeNull()
+        }) 
+        it('should return an error if password wrong',async()=>{
+            jest.spyOn(mockModel, 'findOne').mockImplementation(() => ReturnUser);
+            const response1 = await userService.signIn(validUser)
+            expect(response1).toBeNull()
+        })  
+    })
+    
 });
